5528894e4c86a5afd384eeae040c67fa838d8b7c
Chris Renfrow <dev@chrisrenfrow.me>
2022-02-25 19:05:14 -0800

    Move random word selection to WordList
    And other minor tweaks for readability

--- app/Main.hs
+++ app/Main.hs
@@ -6,14 +6,12 @@ import WordList
 
 import Data.Char (toUpper)
 import Control.Monad.State (StateT, execStateT, lift, modify, get)
-import System.Random (getStdRandom, randomR)
 
 main :: IO ()
 main = do
   wordList <- readWordList "./wordlist.txt"
-  idx <- getStdRandom (randomR (0, length wordList - 1))
-  let answer = wordList !! idx -- Our word to guess
-  let tries = 6                -- The number of tries/guesses
+  answer <- getRandomWord wordList -- Our word to guess
+  let tries = 6                    -- The number of tries/guesses
   putStrLn "Welcome to Wordle CLI!\nPlease enter your first guess:"
   _ <- execStateT (guessSession tries answer) []
   putStrLn "Thanks for playing!"
@@ -24,9 +22,10 @@ guessSession tries answer =
      let match = getMatches answer g
      modify (++ [match])
      gs <- get
-     if all (\(_,x) -> x == InPlace) match
+     if all ((== InPlace) . snd) match
        then lift $ printResults answer gs
        else if length gs < tries
             then do lift $ printGuesses gs
+                    lift $ putStrLn "Enter another guess: "
                     guessSession tries answer
             else lift $ printResults answer gs
--- app/WordList.hs
+++ app/WordList.hs
@@ -1,9 +1,15 @@
 module WordList where
 
 import qualified Data.ByteString.Lazy.Char8 as L
+import System.Random (getStdRandom, randomR)
 
 readWordList :: FilePath -> IO [String]
 readWordList path = do
   contents <- L.readFile path
   return (parse contents)
     where parse = map L.unpack . L.lines
+
+getRandomWord :: [String] -> IO String
+getRandomWord wordList = do
+  idx <- getStdRandom (randomR (0, length wordList - 1))
+  return (wordList !! idx)

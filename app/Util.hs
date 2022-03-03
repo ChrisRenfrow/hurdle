bfdce69562bc1106a79baa9c730ee1b2e3f59c6d
Chris Renfrow <dev@chrisrenfrow.me>
2022-03-03 09:47:02 -0800

    Remove unused utility function

--- app/Util.hs
+++ app/Util.hs
@@ -1,19 +1,6 @@
 module Util
   ( indexed
-  , occur
   ) where
 
 indexed :: [a] -> [(Int, a)]
 indexed xs = zip [0 .. (length xs)] xs
-
-occur :: (Eq e) => e -> [e] -> Int
-t `occur` xs =
-  revFoldl
-    0
-    es
-    (\acc x ->
-       case x == t of
-         True  -> acc + 1
-         False -> acc)
-  where
-    revFoldl acc xs f = foldl f acc xs
